7db40cad5cc40a1a1785d761537110b2309d1b22
Daniel Saewitz <git@saewitz.com>
2025-04-18 00:13:19 -0400

    Fix issue; upgrade packages

---
+++ index.ts
@@ -0,0 +1,55 @@
+import ical from 'ical-generator';
+
+// Read calendar URL from environment variable
+
+export default {
+  async fetch(request: Request, env: Env): Promise<Response> {
+    return await eventHandler(env);
+  }
+};
+
+interface Env {
+  URL: string;
+}
+
+async function eventHandler(env: Env) {
+  const data = await fetch(env.URL);
+  let response = await gatherResponse(data);
+
+  const finalResponse = new Response(response);
+  finalResponse.headers.set('cache-control', 's-maxage=86400'); // 1 day
+  finalResponse.headers.set('content-type', 'text/calendar; charset=UTF-8');
+  finalResponse.headers.set('content-disposition', 'inline');
+
+  return finalResponse;
+}
+
+async function gatherResponse(data: Response) {
+  // Reads the response as JSON
+  const json = await data.json();
+
+  // Create a new calendar
+  const calendar = ical({
+    prodId: '-//PhishNetIcal',
+    name: 'Phish',
+    description: 'Upcoming Phish & Side Project Shows',
+    timezone: 'America/New_York'
+  });
+
+  // For each show, create an event
+  json.data.forEach((show: any) => {
+    // Create a new event
+    calendar.createEvent({
+      summary: show.artist_name,
+      location: `${show.venue} ${show.city}, ${show.state}`,
+      // url: show.permalink,
+      id: show.showid,
+      start: new Date(`${show.showyear}-${show.showmonth}-${show.showday}`),
+      allDay: true,
+    });
+  });
+
+  // Return the calendar as a string
+  console.log(calendar.toString());
+  return calendar.toString();
+}
